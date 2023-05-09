a343b4502150ff68b72564251090640a759e476e
Iasonas Kotsarapoglou <kotsarapoglouiasonas@gmail.com>
2023-05-09 12:26:15 +0300

    Completed

--- tdd-domain-modelling.CSharp.Main/CohortManager.cs
+++ tdd-domain-modelling.CSharp.Main/CohortManager.cs
@@ -8,5 +8,63 @@ namespace tdd_domain_modelling.CSharp.Main
 {
     public class CohortManager
     {
+        public Dictionary<string, int> items = new Dictionary<string, int>()
+        {
+            {"Aplles2",3},
+            {"Oranges2",2},
+            {"Bananas1",7},
+            {"Pineapple3",10},
+            {"Coconut4",5}
+        };
+
+        public bool add(string product, int price)
+        {
+            foreach(KeyValuePair<string, int> kvp in items)
+            {
+                if (kvp.Key.Equals(product))
+                {
+                    return false;
+                }
+            }
+            items.Add(product, price);
+            return true;
+        }
+        public int totalCost()
+        {
+            int total = 0;
+            foreach (KeyValuePair<string, int> item in items)
+            {
+                total += item.Value;
+            }
+            return total;
+        }
+
+        public List<string> Receipt(Dictionary<string, int> items)
+        {
+
+            List<object> objectList = new List<object>();
+            objectList.Add("hi");
+            objectList.Add(1);
+            foreach (object item in objectList)
+            {
+                if (item is string)
+                {
+                    // do something
+                } else if (item is int)
+                {
+                    // do something else
+                }else
+                {
+                    // whatever
+                }
+            }
+            List<string> list = new List<string>();
+            foreach (KeyValuePair<string, int> item in items)
+            {
+                string s1 = item.Key.Remove(item.Key.Length - 1, 1);
+                list.Add("Item: " + s1 + " Quantity: " + item.Key[item.Key.Length - 1] + " Value: " + item.Value);
+            }
+            return list;
+        }
     }
 }
--- tdd-domain-modelling.CSharp.Test/CohortManagerTests.cs
+++ tdd-domain-modelling.CSharp.Test/CohortManagerTests.cs
@@ -1,13 +1,34 @@
-﻿using tdd_domain_modelling.CSharp.Main;
+﻿using NUnit.Framework;
+using tdd_domain_modelling.CSharp.Main;
 
 namespace tdd_domain_modelling.CSharp.Test
 {
+    [TestFixture]
     public class CohortManagerTest
     {
         private CohortManager _core;
         public CohortManagerTest()
         {
             _core = new CohortManager();
+        }
+
+        [TestCase("Aplles2", 3, false)]
+        [TestCase("Juice", 15, true)]
+        [TestCase("Meat", 20, true)]
+        public void addTest(string name, int value, bool expected)
+        {
+            Assert.IsTrue(_core.add(name, value) == expected);
+        }
+
+        [Test]
+        public void totalTest()
+        {
+            int total = 0;
+            foreach(KeyValuePair<string, int> kvp in _core.items)
+            {
+                total += kvp.Value;
+            }
+            Assert.AreEqual(_core.totalCost(), total);
         }
     }
 }
