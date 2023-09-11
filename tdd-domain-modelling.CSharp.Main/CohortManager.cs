8bc925ccb0d6b76862748639f8049cde1875d417
Ävadakedavr0 <dylandd4@hotmail.be>
2023-09-11 16:03:24 +0200

    tests and main project done

--- tdd-domain-modelling.CSharp.Main/CohortManager.cs
+++ tdd-domain-modelling.CSharp.Main/CohortManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,40 @@ namespace tdd_domain_modelling.CSharp.Main
 {
     public class CohortManager
     {
+        public class Basket
+        {
+            public Dictionary<string, int> products = new Dictionary<string, int>();
+            public void addProduct(string productName, int price)
+            {
+                if (!products.ContainsKey(productName))
+                {
+                    products.Add(productName, price);
+                }
+            }
+            public int totalCost()
+            {
+                int total = 0;
+                foreach (var price in products.Values)
+                {
+                    total += price;
+                }
+                return total;
+            }
+        }
+
+        public class Receipt
+        {
+            public Dictionary<string, int> purchasedProducts = new Dictionary<string, int>();
+            public string viewTheReceipt()
+            {
+                List<string> itemList = new List<string>();
+                foreach (var item in purchasedProducts)
+                {
+                    itemList.Add($"{ item.Key} : { item.Value}");
+                }
+                return string.Join(",", itemList);
+            }
+        }
+
     }
 }
--- tdd-domain-modelling.CSharp.Test/CohortManagerTests.cs
+++ tdd-domain-modelling.CSharp.Test/CohortManagerTests.cs
@@ -7,9 +7,43 @@ namespace tdd_domain_modelling.CSharp.Test
     public class CohortManagerTest
     {
         [Test]
-        public void FirstTest()
+        public void AddingProduct()
         {
-            CohortManager core = new CohortManager();
+            //setup
+            CohortManager.Basket basket = new CohortManager.Basket();
+            String product = "Apple";
+            int price = 1;
+            //execute
+            basket.addProduct(product, price);
+            //verify
+            Assert.IsTrue(basket.products.ContainsKey(product)); // expect it to be true
+        }
+        [Test]
+        public void TotalCost()
+        {
+            //setup
+            CohortManager.Basket basket = new CohortManager.Basket();
+            basket.addProduct("AvadakedavrO", 13);
+            basket.addProduct("Harry", 12);
+            //execute
+            int total = basket.totalCost();
+            //verify
+            Assert.AreEqual(25, total); // expect it to be 25
+        }
+
+        [Test]
+        public void Receipt()
+        {
+            //setup
+            CohortManager.Receipt receipt = new CohortManager.Receipt();
+            receipt.purchasedProducts.Add("Lord", 11);
+            receipt.purchasedProducts.Add("Voldemort", 14);
+            //execute
+            String theReceipt = receipt.viewTheReceipt();
+            //verify
+            String expectedReceipt = "Lord : 11,Voldemort : 14";
+            Assert.AreEqual(expectedReceipt, theReceipt); // expect it to be Lord: 11,Voldemort: 14
+
         }
     }
 }
