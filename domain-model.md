1830c6ad5ffa9e612afbae5aecef155452c2bca0
Nikita-vB <n.vanberendonk@gmail.com>
2023-12-01 15:55:48 +0100

    Checkout program + Tests

--- domain-model.md
+++ domain-model.md
@@ -1,1 +1,34 @@
 #Domain Models In Here
+
+### Exercise
+
+Follow the same process as above to translate these two user stories into domain models.
+
+
+## One
+```
+As a supermarket shopper,
+So that I can pay for products at checkout,
+I'd like to be able to know the total cost of items in my basket.
+```
+
+| Classes         | Methods                                                          | Scenario                         | Outputs |
+|-----------------|------------------------------------------------------------------|----------------------------------|---------|
+| `Checkout.cs`   | `List<Item> myBasket` with items + price & quantity				 | Store items + price + quantity   | List    |
+| `Checkout.cs`   | Method `AddToBasket(item,price,quantity)`						 | Store items + price + quantity   | Void    |
+| `Checkout.cs`   | Method `CheckoutSum(basket)`									 | Sum cost for each item			| Int     |
+
+
+## Two
+```
+As an organised individual,
+So that I can evaluate my shopping habits,
+I'd like to see an itemised receipt that includes the name and price of the products
+I bought as well as the quantity, and a total cost of my basket.
+```
+
+| Classes         | Methods																		     | Scenario                       | Outputs |
+|-----------------|----------------------------------------------------------------------------------|--------------------------------|---------|
+| `Checkout.cs  ` | Method `Receipt()`																 | Upon request, return receipt   | String  |
+|				  |	- the returns the basket, quantity of each item and								 |								  |         |
+|				  | - sum total (CheckoutSum() added as final item to list)							 |								  |			|
--- tdd-domain-modelling.CSharp.Test/CohortManagerTests.cs
+++ tdd-domain-modelling.CSharp.Test/CohortManagerTests.cs
@@ -1,15 +1,55 @@
 ﻿using NUnit.Framework;
+using System.Collections;
+using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
 using tdd_domain_modelling.CSharp.Main;
+using static tdd_domain_modelling.CSharp.Main.Basket;
+//using NUnit.Framework.Legacy;
 
 namespace tdd_domain_modelling.CSharp.Test
 {
     [TestFixture]
     public class CohortManagerTest
     {
+
         [Test]
-        public void FirstTest()
+        public void BasketSum()
         {
-            CohortManager core = new CohortManager();
+            //arrange
+            //instance a new basket
+            Basket basket = new Basket();
+            
+            //add items to basket (new items are created when adding)
+            //AddToBasket(item,price,quantity)`
+            basket.AddToBasket("Cucumber", 1.99F, 1);
+            basket.AddToBasket("Bread", 2.50F, 2);
+            basket.AddToBasket("Bread", 1.50F, 1);
+            float expected = (1.99F * 1) + (2.50F *2) + (1.50F * 1);
+
+            //act
+            float result = basket.CheckoutSum();
+
+            //assert
+            Assert.That(result, Is.EqualTo(expected));
         }
+
+        [Test]
+        public void TestReceipt()
+        {
+            //arrange
+            Basket basket = new Basket();
+            //AddToBasket(item,price,quantity)`
+            basket.AddToBasket("Cucumber", 1.99F, 1);
+            basket.AddToBasket("Bread", 2.50F, 2);
+            float totalCost = 1.99F + (2.50F * 2);
+
+            //act
+            string myReceipt = basket.GetReceipt();
+
+            //assert
+            //Assert.IsTrue(basket.myBasket.Count != 0); //passes -> the items are put in the basket
+            Assert.IsTrue(myReceipt.Contains($"Total {totalCost}"));
+        }
+
     }
 }
