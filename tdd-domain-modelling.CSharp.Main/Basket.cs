26fffe03d23cc72b45a0f499386a04874470ab8b
Stian Gaustad <stian.gaustad@no.experis.com>
2024-01-12 13:44:09 +0100

    Implemented test for basket add

---
+++ tdd-domain-modelling.CSharp.Main/Basket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_domain_modelling.CSharp.Main
+{
+    public class Basket
+    {
+        Dictionary<string, int> items = new Dictionary<string, int>();
+
+        public bool add(string product, int price) 
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+};
---
+++ tdd-domain-modelling.CSharp.Test/Exercise2Tests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using tdd_domain_modelling.CSharp.Main;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_domain_modelling.CSharp.Test
+{
+    public class Tests
+    {
+        [TestFixture]
+        public class Exercise2Tests
+        {
+            [SetUp]
+            public void Setup() 
+            { 
+
+            }
+
+            [Test]
+            public void AddProductTest() 
+            {
+                // Arrange
+                Basket basket = new Basket();
+                string productName = "Bread";
+                int productPrice = 20;
+
+                // Act
+                bool res = basket.add(productName, productPrice);
+
+                // Assert
+                Assert.That(true, Is.EqualTo(res));
+            }
+
+        }
+    }
+}
