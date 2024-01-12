4590ce7f88f8e88ca0b94ee56505e34cbb412fd1
Benjamin A. E. Darici <benjamin.darici@no.experis.com>
2024-01-12 13:24:37 +0100

    Benjamin A.E. Darici C#EX3 - think this is right?

---
+++ tdd-domain-modelling.CSharp.Main/Basket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_domain_modelling.CSharp.Main
+{
+    public class Basket
+    {
+
+        private Dictionary<String, int> _items = new Dictionary<String, int>();
+        public Basket() { }
+        public bool Add(string name, int price) 
+        {
+            if(_items.ContainsKey(name))
+            {
+                return false;
+            }
+            _items.Add(name, price);
+
+            return true; 
+        }
+
+        public int Total()
+        {
+            int total = 0;
+            foreach(var item in _items)
+            {
+                total += item.Value;
+            }
+            return total;
+        }
+    }
+}
---
+++ tdd-domain-modelling.CSharp.Test/BasketTests.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tdd_domain_modelling.CSharp.Main;
+
+namespace tdd_domain_modelling.CSharp.Test
+{
+    internal class BasketTests
+    {
+
+        [TestCase("Apples", 3, true)]
+        [TestCase("Bread", 4, true)]
+        public void AddTest1(string s, int i, bool expected)
+        {
+            //  Arrange - set up test values
+            Basket basket = new Basket();
+
+
+            //  Act - use the fucntion we want to test
+            bool result = basket.Add(s, i);
+
+            //  Assert - check the results
+            Assert.True(result);
+
+        }
+
+        [TestCase("Apples", 2, false)]
+        [TestCase("Bread", 4, false)]
+        public void AddTest2(string s, int i, bool expected)
+        {
+            //  Arrange - set up test values
+            Basket basket = new Basket();
+
+            //  Act - use the fucntion we want to test
+            basket.Add(s, i);
+            bool result = basket.Add(s, i);
+
+            //  Assert - check the results
+            Assert.IsTrue(result == expected);
+
+        }
+
+        [Test]
+        public void TotalTest()
+        {
+            //  Arrange - set up test values
+            Basket basket = new Basket();
+            basket.Add("Apple", 3);
+            basket.Add("Bread", 4);
+
+
+            //  Act - use the fucntion we want to test
+            int result = basket.Total();
+
+            //  Assert - check the results
+            Assert.That(result, Is.EqualTo(7));
+
+        }
+    }
+}
