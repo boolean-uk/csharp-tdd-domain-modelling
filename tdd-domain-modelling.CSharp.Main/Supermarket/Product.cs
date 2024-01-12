2bee80bef4ae2211b11217ba76159230bc32801c
Tølløv Aresvik <tollov.aresvik@no.experis.com>
2024-01-12 16:03:50 +0100

    First successful test fail

---
+++ tdd-domain-modelling.CSharp.Main/Supermarket/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_domain_modelling.CSharp.Main.Supermarket
+{
+    public class Product
+    {
+
+    }
+}
