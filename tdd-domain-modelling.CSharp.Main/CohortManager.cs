910839ddab3ce4514b5668b7e2d1b12ea1475c5a
Klara Andersson <klara.andersson@se.experis.com>
2024-01-12 11:04:55 +0100

    Methods and tests implemented

--- tdd-domain-modelling.CSharp.Main/CohortManager.cs
+++ tdd-domain-modelling.CSharp.Main/CohortManager.cs
@@ -8,5 +8,37 @@ namespace tdd_domain_modelling.CSharp.Main
 {
     public class CohortManager
     {
+        private Dictionary<string, int> items;
+
+        public CohortManager()
+        {
+            items = new Dictionary<string, int>();
+        }
+
+        public bool add(string product, int price)
+        {
+            bool notInBasket = true;
+
+            for (int i = 0; i < items.Count(); i++)
+            {
+                if (items.ContainsKey(product))
+                    notInBasket = false;
+            }
+
+            if (notInBasket)
+                items.Add(product, price);
+
+            return notInBasket;
+        }
+
+        public int total()
+        {
+            int totalCost = 0;
+
+            for (int i = 0; i < items.Count(); i++)
+                totalCost += items.ElementAt(i).Value;
+
+            return totalCost;
+        }
     }
 }
--- tdd-domain-modelling.CSharp.Test/CohortManagerTests.cs
+++ tdd-domain-modelling.CSharp.Test/CohortManagerTests.cs
@@ -6,10 +6,52 @@ namespace tdd_domain_modelling.CSharp.Test
     [TestFixture]
     public class CohortManagerTest
     {
-        [Test]
-        public void FirstTest()
+        CohortManager manager;
+
+        [SetUp]
+        public void SetUp()
         {
-            CohortManager core = new CohortManager();
+            manager = new CohortManager();
+        }
+
+        [Test]
+        public void AddingItemsToBasket()
+        {
+            bool result = manager.add("Cheese", 30);
+            bool result2 = manager.add("Ham", 40);
+
+            Assert.IsTrue(result2);
+        }
+
+        [Test]
+        public void AddingDoubleItemsToBasket()
+        {
+            bool result = manager.add("Cheese", 30);
+            bool result2 = manager.add("Cheese", 40);
+
+            Assert.IsFalse(result2);
+        }
+
+        [Test]
+        public void TestingTotalValue()
+        {
+            manager.add("Ham", 40);
+            manager.add("Cheese", 30);
+            manager.add("Milk", 50);
+            int totalCost = manager.total();
+
+            Assert.That(120, Is.EqualTo(totalCost));
+        }
+
+        [Test]
+        public void TestingTotalDoubles()
+        {
+            manager.add("Ham", 40);
+            manager.add("Ham", 30);
+            manager.add("Milk", 50);
+            int totalCost = manager.total();
+
+            Assert.That(90, Is.EqualTo(totalCost));
         }
     }
 }
