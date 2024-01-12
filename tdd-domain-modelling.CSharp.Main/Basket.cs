bd9c5948061f3d4069eeab24a0ce0ecdb6934c25
Oneal <oneallane2808@gmail.com>
2024-01-12 15:58:09 +0100

    done

---
+++ tdd-domain-modelling.CSharp.Main/Basket.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_domain_modelling.CSharp.Main
+{
+    public class Basket
+{
+        public Dictionary<string, int> _shoppingBasket;
+        public Basket() 
+        {
+            _shoppingBasket = new Dictionary<string, int>();
+        }
+
+        public bool Add(string item, int price)
+        {
+            if (!_shoppingBasket.ContainsKey(item))
+            {
+                _shoppingBasket[item] = price;
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        public int Total()
+        {
+            int total = 0;
+            foreach (var item in _shoppingBasket)
+            {
+                total += item.Value;
+            }
+            return total;
+        }
+    }
+
+}
---
+++ tdd-domain-modelling.CSharp.Test/BasketTest.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_domain_modelling.CSharp.Main
+{
+    [TestFixture]
+    public class BasketTest
+{
+        [Test]
+        public void AddTest()
+        {
+            var basket = new Basket();
+            string item1 = "six-pack of apples";
+            int price1 = 65;
+            string item2 = "Lego Death Star";
+            int price2 = 7899;
+
+            basket.Add(item2, price2);
+
+
+            Assert.That(price2 == basket._shoppingBasket[item2] , Is.True);
+        }
+
+        [Test]
+        public void TotalTest()
+        {
+            var basket = new Basket();
+            string item1 = "six-pack of apples";
+            int price1 = 65;
+            string item2 = "Lego Death Star";
+            int price2 = 7899;
+
+            basket.Add(item1, price1);
+            basket.Add(item2, price2);
+
+
+            Assert.That(price2 == basket._shoppingBasket[item2], Is.True);
+            Assert.That(basket._shoppingBasket.ContainsKey(item1), Is.True);
+            Assert.AreEqual(basket.Total(), 7964);
+        }
+
+
+
+    }
+}
