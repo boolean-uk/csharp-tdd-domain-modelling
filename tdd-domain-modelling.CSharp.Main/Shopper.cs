9dba6b50c285255eee8b962c31360f85f9958e40
Benjamin Samuelsen Austestad <ben_7aus@hotmail.com>
2024-01-12 13:47:52 +0100

    Fully implement Shopper and tests

--- tdd-domain-modelling.CSharp.Main/Shopper.cs
+++ tdd-domain-modelling.CSharp.Main/Shopper.cs
@@ -6,19 +6,41 @@ using System.Threading.Tasks;
 
 namespace tdd_domain_modelling.CSharp.Main
 {
-    public class Shopper
+    public class Shopper(float money)
     {
         private List<Product> _cupboard = new();
         private List<Product> _basket = new();
+        private float _money = money;
+
+        public float Money
+        {
+            get => _money;
+        }
 
         public void Restock()
         {
             _cupboard.AddRange(_basket);
+            _basket.Clear();
         }
 
         public void Add(Product product)
         {
             _basket.Add(product);
+        }
+
+        public void Pay()
+        {
+            _money -= Total();
+        }
+
+        public float Total()
+        {
+            float total = 0;
+            foreach (Product product in _basket)
+            {
+                total += product.Price;
+            }
+            return total;
         }
 
         public List<Product> Cupboard { get { return _cupboard; } }
--- tdd-domain-modelling.CSharp.Test/ShopperTests.cs
+++ tdd-domain-modelling.CSharp.Test/ShopperTests.cs
@@ -11,17 +11,26 @@ namespace tdd_domain_modelling.CSharp.Test
     [TestFixture]
     internal class ShopperTests
     {
+
+        private Shopper _shopper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _shopper = new Shopper(100);
+        }
+
         [Test]
         [TestCase("Milk", 3)]
         [TestCase("Apple", 5)]
         [TestCase("Coffee", 10)]
         public void RestockTest(string name, float price)
         {
-            Shopper shopper = new();
             Product product = new(name, price);
-            shopper.Basket.Add(product);
-            shopper.Restock();
-            Assert.That(shopper.Cupboard.Contains(product), Is.True);
+            _shopper.Basket.Add(product);
+            _shopper.Restock();
+            Assert.That(_shopper.Cupboard.Contains(product), Is.True);
+            Assert.That(_shopper.Basket.Count, Is.EqualTo(0));
         }
 
         [Test]
@@ -30,10 +39,69 @@ namespace tdd_domain_modelling.CSharp.Test
         [TestCase("Coffee", 10)]
         public void AddTest(string name, float price)
         {
-            Shopper shopper = new();
             Product product = new(name, price);
-            shopper.Add(product);
-            Assert.That(shopper.Basket.Contains(product), Is.True);
+            _shopper.Add(product);
+            Assert.That(_shopper.Basket.Contains(product), Is.True);
+        }
+
+        [Test]
+        public void PayTest()
+        {
+            List<Product> products = new() 
+            {
+                new Product("Eggs", 10),
+                new Product("Tea", 15),
+                new Product("Milk", 5)
+            };
+            foreach (var product in products)
+            {
+                _shopper.Add(product);
+            }
+            _shopper.Pay();
+            Assert.AreEqual(70, _shopper.Money);
+        }
+
+        [Test]
+        public void TotalTest()
+        {
+            List<Product> products = new()
+            {
+                new Product("Eggs", 10),
+                new Product("Tea", 15),
+                new Product("Milk", 5),
+                new Product("Coffe", 20)
+            };
+            foreach (var product in products)
+            {
+                _shopper.Add(product);
+            }
+            Assert.AreEqual(50, _shopper.Total());
+        }
+
+        [Test]
+        public void ShopperFullScenarioTest()
+        {
+            List<Product> products = new()
+            {
+                new Product("Eggs", 10),
+                new Product("Tea", 15),
+                new Product("Milk", 5),
+                new Product("Coffe", 20)
+            };
+            foreach (var product in products)
+            {
+                _shopper.Add(product);
+            }
+            Assert.AreEqual(50, _shopper.Total());
+            
+            _shopper.Pay();
+            Assert.AreEqual(50, _shopper.Money);
+
+            _shopper.Restock();
+            foreach (var product in products)
+            {
+                Assert.That(_shopper.Cupboard.Contains(product));
+            }           
         }
     }
 }
