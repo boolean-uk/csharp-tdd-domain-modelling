dc7fe74e768c03405a75e1705ce33d00f17afd94
Pia Loana Mullenbach <pia.loana.mullenbach@dk.experis.com>
2024-01-14 11:49:34 +0100

    finished exercise

---
+++ tdd-domain-modelling.CSharp.Test/BasketTest.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using tdd_domain_modelling.CSharp.Main;
+
+namespace tdd_domain_modelling.CSharp.Test
+{
+    [TestFixture]
+    public class BasketTest
+    {
+        private Basket _basket;
+
+        [SetUp]
+
+        public void SetUp()
+        {
+            _basket = new Basket();
+            _basket.add("Bread", 2);
+            _basket.add("Bread", 2);
+            _basket.add("Water", 1);
+            _basket.add("Cheese", 3);
+            _basket.add("Milk", 1);
+        }
+
+
+        [Test]
+
+        public void TotalTest()
+        {
+            int totalPrice = _basket.total();
+            Assert.That(totalPrice, Is.EqualTo(9));
+        }
+
+        [Test]
+        public void EmptyTest()
+        {
+            _basket.clear();
+            int totalPrice = _basket.total();
+            Assert.That(totalPrice, Is.EqualTo(0));
+        }
+
+
+        [Test]
+        public void TotalTest2()
+        {
+            _basket.remove("Bread");
+            int totalPrice = _basket.total();
+            Assert.That(totalPrice, Is.EqualTo(5));
+        }
+
+    }
+}
--- tdd-domain-modelling.CSharp.Test/CohortManagerTests.cs
+++ tdd-domain-modelling.CSharp.Test/CohortManagerTests.cs
@@ -6,10 +6,31 @@ namespace tdd_domain_modelling.CSharp.Test
     [TestFixture]
     public class CohortManagerTest
     {
-        [Test]
-        public void FirstTest()
+        private CohortManager _manager;
+        private List<string> _cohort;
+
+        [SetUp]
+
+        public void SetUp()
         {
-            CohortManager core = new CohortManager();
+            _manager = new CohortManager();
+            _cohort = new List<string>();
+            _cohort.Add("Kindergarden");
+            _cohort.Add("Cohort");
+            _cohort.Add("Cohort 3");
+            _cohort.Add("Cohort 4");
+        }
+
+
+        [TestCase("Kindergarden", true)]
+        [TestCase("cohort", true)]
+        [TestCase("Cohort 5", false)]
+        [TestCase("", false)]
+
+        public void FirstTest(string nameCohort, bool result)
+        {
+            bool actualResult = _manager.Search(_cohort, nameCohort);
+            Assert.That(actualResult, Is.EqualTo(result));
         }
     }
 }
