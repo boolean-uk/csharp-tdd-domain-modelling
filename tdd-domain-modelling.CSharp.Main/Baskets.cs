5c7935dd9a0c1799095c2e699fdd2f599bdaf06e
Aziz <azizzafar1991@gmail.com>
2024-01-14 14:17:00 +0100

    Aziz Zafar C#EX3

---
+++ tdd-domain-modelling.CSharp.Main/Baskets.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_domain_modelling.CSharp.Main
+{
+    public class Basket
+    {
+        private Dictionary<string, int> items;
+
+        public Basket()
+        {
+            items = new Dictionary<string, int>();
+        }
+
+        public bool AddProduct(string productName, int price)
+        {
+            if (!items.ContainsKey(productName))
+            {
+                items[productName] = price;
+                return true; // Product added successfully
+            }
+            else
+            {
+                return false; // Product already in the basket
+            }
+        }
+
+        public int CalculateTotal()
+        {
+            int total = 0;
+            foreach (var item in items)
+            {
+                total += item.Value;
+            }
+            return total;
+        }
+    }
+}
---
+++ tdd-domain-modelling.CSharp.Test/BasketTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using tdd_domain_modelling.CSharp.Main;
+
+namespace tdd_domain_modelling.CSharp.Test
+{
+    [TestFixture]
+    public class BasketTest
+    {
+
+
+        [Test]
+        public void Add_NewItemToBasket()
+        {
+            Basket basket = new Basket();
+            bool result = basket.AddProduct("Milk", 2); // Adding 2 units of Milk to the basket
+            Assert.IsTrue(result);
+        }
+
+
+        [Test]
+        public void Add_ExistingItemToBasket()
+        {
+            Basket basket = new Basket();
+            basket.AddProduct("Bread", 3); // Adding 3 units of Bread to the basket initially
+            bool result = basket.AddProduct("Bread", 1); // Trying to add more Bread to the basket
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void CalculateTotal_Total()
+        {
+            // Arrange
+            Basket basket = new Basket();
+            basket.AddProduct("Apple", 2);
+            basket.AddProduct("Banana", 3);
+
+            // Act
+            int total = basket.CalculateTotal();
+
+            // Assert
+            Assert.AreEqual(5, total);
+        }
+    }
+}
