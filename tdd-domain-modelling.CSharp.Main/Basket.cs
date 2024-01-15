448f242ddfd0594b422580d0d555c513b64898df
Elsa Lossius <elsa.lossius@no.experis.com>
2024-01-15 09:38:20 +0100

    Failing test

---
+++ tdd-domain-modelling.CSharp.Main/Basket.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace td_domain_modelling.CSharp.Main
+{
+    public class Basket
+    {
+        private Dictionary<string, int> _items = new Dictionary<string, int>();
+
+        public Basket()
+        {
+
+        }
+
+        public bool Add(string product, int price)
+        {
+            _items.Add(product, price);
+
+            return _items.Count > 0 ? true : false;
+        }
+
+        public int Total()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, int> items { get { return _items;  } set { _items = value; } }
+
+
+    } 
+}
+
+
+    
+
