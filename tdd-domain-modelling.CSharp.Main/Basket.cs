cf5d9311c7847cdab810e2f31c968c9d6931857f
Jacob Chen Jensen <jacob.chen.jensen@dk.experis.com>
2024-01-15 13:41:16 +0100

    finished

---
+++ tdd-domain-modelling.CSharp.Main/Basket.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_domain_modelling.CSharp.Main
+{
+
+    public struct Item
+    {
+        public string itemName { get; set; }
+        public int itemPrice { get; set; }
+
+        public Item(string name, int price)
+        {
+            itemName = name;
+            itemPrice = price;
+        }
+    }
+    public class Basket
+    {
+
+        public List<Item> Items { get; set; }
+
+        public Basket()
+        {
+            Items = new List<Item>();
+        }
+
+
+        public bool addItem(string item, int price)
+        {
+            bool result;
+            Item temp = new Item(item,price);
+
+            if (Items.Contains(temp) == true)
+            {
+                result = false;
+            }
+            else
+            {
+                Items.Add(temp);
+                result = true;
+            }
+
+            return result;
+        }
+        public int totalCost()
+        {
+            int total = 0;
+
+            foreach (Item item in Items)
+            {
+                total += item.itemPrice;
+            }
+
+
+            return total;
+        }
+
+
+    }
+}
---
+++ tdd-domain-modelling.CSharp.Test/BasketTests.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using tdd_domain_modelling.CSharp.Main;
+
+namespace tdd_domain_modelling.CSharp.Test
+{
+    [TestFixture]
+    public class BasketTests
+    {
+        [Test]
+        public void FirstTest()
+        {
+            Basket basket = new Basket();
+
+            bool addTest = basket.addItem("Milk", 3);
+            
+            Assert.IsTrue(addTest);
+        }
+        [Test]
+        public void SecondTest()
+        {
+            Basket basket = new Basket();
+
+            basket.addItem("Milk", 3);
+            bool addTest = basket.addItem("Milk", 3);
+            
+            Assert.IsFalse(addTest);
+
+        }
+        [Test]
+        public void ThirdTest()
+        {
+            Basket basket = new Basket();
+
+            basket.addItem("Milk", 3);
+
+            Assert.IsTrue(basket.totalCost() == 3);
+        }
+        [Test]
+        public void FourthTest()
+        {
+            Basket basket = new Basket();
+            basket.addItem("Milk", 3);
+            basket.addItem("Flour", 2);
+            basket.addItem("Egg", 5);
+
+            Assert.IsTrue(basket.totalCost() == 10);
+        }
+        [Test]
+        public void FifthTest()
+        {
+            Basket basket = new Basket();
+            basket.addItem("Milk", 3);
+            basket.addItem("Milk", 3);
+            Assert.IsTrue(basket.totalCost() == 3);
+        }
+
+    }
+}
