17ac6f9d95f9ac92fc3d5184182e808370c5644b
Mateusz Pietrzak <matti3301@gmail.com>
2024-01-15 08:26:37 +0100

    Completed all tasks

---
+++ tdd-domain-modelling.CSharp.Test/BasketTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tdd_domain_modelling.CSharp.Main;
+
+namespace tdd_domain_modelling.CSharp.Test
+{
+    public class BasketTests
+    {
+        [SetUp]
+        public void SetUp() { }
+
+        [Test]
+        public void AddItemToBasket()
+        {
+            // Arrange
+            Basket basket = new Basket();
+
+            // Act
+            basket.Add("Bread", 10);
+
+            // Assert
+            Assert.IsTrue(basket.items.ContainsKey("Bread"));
+            Assert.AreEqual(10, basket.items["Bread"]);
+        }
+
+        [Test]
+        public void AddExistingItemToBasket()
+        {
+            // Arrange
+            Basket basket = new Basket();
+
+            // Act
+            basket.Add("Bread", 10);
+
+            // Assert
+            Assert.Throws<Exception>(() => basket.Add("Bread", 15));
+        }
+
+    }
+}
