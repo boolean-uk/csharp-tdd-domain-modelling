5279185d262061b4b741d1b594f2dee27130adc9
Linus Willmont <linus.willmont@se.experis.com>
2024-01-19 21:19:04 +0100

    Done

--- domain-model.md
+++ domain-model.md
@@ -1,1 +1,60 @@
 #Domain Models In Here
+
+```
+As a supermarket shopper,
+So that I can pay for products at checkout,
+I'd like to be able to know the total cost of items in my basket.
+```
+
+```C# 
+struct Item
+	PROPERTIES:
+	public string itemName {get}
+	public float cost {get}
+
+	METHODS:
+		public Item(string itemName, float cost)
+
+Class Basket
+	properties:
+	public List<item> items {get}
+
+	methods:
+	public Basket() // initialize items list
+	public void AddItem(string itemName, float cost)
+	public float TotalCost(List<Item>) return the total cost of the basket
+```
+
+
+```
+As an organised individual,
+So that I can evaluate my shopping habits,
+I'd like to see an itemised receipt that includes the name and price of the products
+I bought as well as the quantity, and a total cost of my basket.
+
+```
+
+``` C#
+struct Item
+	PROPERTIES:
+	public string itemName {get}
+	public float cost {get}
+
+	METHODS:
+		public Item(string itemName, float cost)
+
+Class Basket
+	properties:
+	public List<item> items {get}
+
+	methods:
+	public Basket() // initialize items list
+
+	public void AddItem(string itemName, float cost)
+
+	public float TotalCost(List<Item>) return the total cost of the basket
+
+	public List<string> Receipt(List<item> items) 
+		returns a list with:| Name | Price | Quantity |
+		Last index of list  | TotalCost()             |
+```
---
+++ tdd-domain-modelling.CSharp.Main/Basket.cs
@@ -0,0 +1,62 @@
+﻿namespace tdd_domain_modelling.CSharp.Main
+{
+    public class Basket
+    {
+        public List<Item> items { get; }
+
+        public Basket()
+        {
+            items = new List<Item>();
+        }
+
+        public void AddItem(string itemName, float price)
+        {
+            items.Add(new Item(itemName, price));
+        }
+
+        public float TotalCost()
+        {
+            float totalCost = 0;
+            foreach (var item in items)
+            {
+                totalCost += item.Cost;
+            }
+            return totalCost;
+        }
+
+        public List<string> Receipt()
+        {
+            List<string> receipt = new List<string>();
+            Dictionary<Item, float> itemQuantity = new Dictionary<Item, float>();
+            foreach (var item in items)
+            {
+                if (!itemQuantity.ContainsKey(item))
+                {
+                    itemQuantity.Add(item, 1);
+                }
+                else
+                {
+                    itemQuantity[item]++;
+                }
+            }
+
+            foreach (var item in itemQuantity)
+            {
+                receipt.Add($"|{item.Key.ItemName}|{item.Key.Cost}|x{item.Value}");
+            }
+            receipt.Add($"Total cost: {TotalCost()}");
+            return receipt;
+        }
+    }
+
+    public struct Item
+    {
+        public string ItemName { get; }
+        public float Cost { get; }
+        public Item(string itemName, float cost)
+        {
+            ItemName = itemName;
+            Cost = cost;
+        }
+    }
+}
