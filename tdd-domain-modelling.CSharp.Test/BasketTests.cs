ded1ef479988e6f954436bf707dc0317fc0c7a8f
Rose van der Giessen <Rose van der Giessen>
2024-01-29 14:33:31 +0100

    ex 2 complete - no refactor

---
+++ tdd-domain-modelling.CSharp.Test/BasketTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using NUnit.Framework;
+using tdd_domain_modelling.CSharp.Main;
+
+namespace tdd_domain_modelling.CSharp.Test
+{
+    [TestFixture]
+    public class BasketTest
+    {
+
+        [Test]
+        public void TestReturnTrueIfItemIsNotInBasket()
+        {
+            Basket basket = new Basket();
+            basket._items.Add("Milk", 2);
+            Assert.IsTrue(basket.AddItem("Eggs", 6));
+        }
+
+        [Test]
+        public void TestReturnFalseIfItemIsInBasket()
+        {
+            Basket basket = new Basket();
+            basket._items.Add("Milk", 2);
+            Assert.IsFalse(basket.AddItem("Milk", 5));
+        }
+
+        [Test]
+        public void TestIfItemWasAdded()
+        {
+            Basket basket = new Basket();
+            basket.AddItem("Milk", 2);
+            Assert.IsTrue(basket._items.ContainsKey("Milk"));
+        }
+
+        [Test]
+        public void TestTotalCostOfBasket()
+        {
+            Basket basket = new Basket();
+            basket._items.Add("Milk", 2);
+            basket._items.Add("Eggs", 1);
+            Assert.AreEqual(3, basket.Total());
+        }
+
+    }
+}
