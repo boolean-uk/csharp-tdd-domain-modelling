66e017cea825026a68531a22668bb59cb0485bc2
Victor <victor.adamson@se.experis.com>
2024-02-08 12:30:40 +0100

    Implemented solution for addItem, now test passes.

--- tdd-domain-modelling.CSharp.Main/Basket.cs
+++ tdd-domain-modelling.CSharp.Main/Basket.cs
@@ -12,7 +12,13 @@ namespace tdd_domain_modelling.CSharp.Main
         public Dictionary<string, int> items = new Dictionary<string, int>();
         public bool addItem(string name, int price)
         {
-            throw new NotImplementedException();
+            if (items.ContainsKey(name))
+            {
+                return false;
+            }
+            items[name] = price;
+            totalCost += price;
+            return true;
         }
         public int total()
         {
