0ea9dca4a0106b96789a5e4dde27aaae5685bcc3
Rose Breedveld <rosebreedveld@hotmail.com>
2024-03-05 17:26:40 +0100

    Rose Breedveld tdd-domain-modling core

---
+++ tdd-domain-modelling.CSharp.Main/Basket.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_domain_modelling.CSharp.Main
+{
+        public class Basket
+    {
+        public Dictionary<string, double> items = new Dictionary<string, double>(); 
+
+        public Basket()
+        {
+
+        }
+
+        public bool addItemsToBasket(string itemName, double price)
+        {
+            // if itemName is not in items Dicionary, add itemName and Price to items Dicionary
+            if (!items.ContainsKey(itemName))
+            {
+                items.Add(itemName, price);
+                return true;
+            }
+            return false;
+        }
+
+        
+        public double TotalCostsBasket()
+        {
+            double totalCosts = 0;
+            foreach (var item in items)
+            {
+                totalCosts += item.Value;
+            }
+            return totalCosts;
+        }
+    }
+}
---
+++ tdd-domain-modelling.CSharp.Test/BasketTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using tdd_domain_modelling.CSharp.Main;
+
+namespace tdd_domain_modelling.CSharp.Test
+{
+    [TestFixture]
+    public class BasketTests
+    {
+        private Basket _basket;
+        [SetUp] 
+
+        public void Setup()
+        {
+            // creates new basket for each test.
+            _basket = new Basket();
+            _basket.addItemsToBasket("Eggs", 1.5d);
+            _basket.addItemsToBasket("Butter", 1.0d);
+            _basket.addItemsToBasket("Tomatoes", 2.5d);
+        }
+
+        
+        [Test]
+        public void AddItemsToBasket() 
+        {
+            // execute add items to basket
+            bool addedItems = _basket.addItemsToBasket("Milk", 2d);
+
+            // check if added items ("Eggs, 1.5d) is in basket -> is true.
+            Assert.That(addedItems, Is.True);
+        }
+
+        [Test]
+        public void TestTotalCostsBasket()
+        {
+            double totalCosts = _basket.TotalCostsBasket();
+
+            Assert.That(totalCosts, Is.EqualTo(5));
+        }
+    }
+}
